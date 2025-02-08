a785893af299c225a89f1237cb5ab14c3657ee26
VIkash Kumar <103258656+vikash-mdh@users.noreply.github.com>
2025-02-09 00:11:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+	const strongs = document.querySelectorAll("strong");
+	strongs.forEach(strong => {
+		strong.style.color = ("green");
+	});
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	const strongs = document.querySelectorAll("strong");
+	strongs.forEach(strong => {
+		strong.style.color = ("black");
+	});
     
 }
